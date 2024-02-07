158541aff3a99b20e81012f5ec07055a0b886bc2
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-02-06 20:21:42 -0800

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by NSPC201admin on 2024/02/06.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
