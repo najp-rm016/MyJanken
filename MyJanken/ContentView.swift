bd4f463d8a67a9126a96d343c4b0fe9bd8f14833
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-03-07 15:45:48 +0900

    福嶋さんにご確認頂くためにコミット

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answerNumber = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack{
+            if answerNumber == 0{
+                Spacer()
+                Text("これからじゃんけんをします")
+                    .padding(.bottom)
+            } else if answerNumber == 1{
+                Image("gu")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Text("グー")
+                    .padding(.bottom)
+                
+            } else if answerNumber == 2{
+                Image("choki")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Text("チョキ")
+                    .padding(.bottom)
+            } else {
+                Image("pa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Text("パー")
+                    .padding(.bottom)
+            }
+            
+            Button(action: {
+                
+                var newAnswerNunber = 0
+                
+                repeat{
+                    newAnswerNunber = Int.random(in: 1...3)
+                } while answerNumber == newAnswerNunber
+                
+                answerNumber = newAnswerNunber
+            }){
+                Text("じゃんけんをする！")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(Color.pink)
+                    .foregroundColor(Color.white)
+            }
         }
-        .padding()
+        
     }
 }
 
